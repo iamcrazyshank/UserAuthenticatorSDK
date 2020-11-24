d056b4f0f3a448653b17fb14dc7229c6cee1c9fa
Shashank <iamcrazyshank@gmail.com>
2020-11-24 12:48:29 +0000

    Initial Commit

---
+++ UserAuthenticatorSDK/UserAuthenticatorSDK.h
@@ -0,0 +1,18 @@
+//
+//  UserAuthenticatorSDK.h
+//  UserAuthenticatorSDK
+//
+//  Created by Shashank Chandran on 24/11/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UserAuthenticatorSDK.
+FOUNDATION_EXPORT double UserAuthenticatorSDKVersionNumber;
+
+//! Project version string for UserAuthenticatorSDK.
+FOUNDATION_EXPORT const unsigned char UserAuthenticatorSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UserAuthenticatorSDK/PublicHeader.h>
+
+
